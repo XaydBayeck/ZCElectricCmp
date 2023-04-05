0a9361037b7c37529ca90ed5385ae5ab3e094590
Sid <SidBayeck@outlook.com>
2023-04-06 00:15:19 +0800

    Correctly use segment display sample from pb1

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 
 use defmt::info;
 use embassy_executor::{main, task, Spawner};
-use embassy_stm32::{peripherals::*, Config, time::Hertz};
-use embassy_time::{Duration, Timer};
+use embassy_stm32::{adc::Adc, pac::RCC, peripherals::*, time::Hertz, Config};
+use embassy_time::{Delay, Duration, Timer};
 use liquidled_testrs::{
     led::{Led, LedState},
-    segements::Segments,
+    segements::{self, Segments},
 };
 
 use {defmt_rtt as _, panic_probe as _};
@@ -27,6 +27,8 @@ async fn blink(pc7: PC7, pc8: PC8, pc9: PC9) {
 
 #[task]
 async fn display(
+    adc1: ADC1,
+    mut pb1: PB1,
     pb3: PB3,
     pb4: PB4,
     pb5: PB5,
@@ -37,10 +39,27 @@ async fn display(
 ) {
     let mut segmts = Segments::new(pb3, pb4, pb5, pc10, pc11, pc12, fresh_frq);
 
-    let numbers = [0, 2, 1, 4, 3, 2, 6, 7];
+    let mut adc = Adc::new(adc1, &mut Delay);
+
+    let mut numbers = [18usize, 0, 0, 0, 0, 0, 0, 0];
+
+    let mut vrefint = adc.enable_vref(&mut Delay);
+    let vrefint_sample = adc.read(&mut vrefint);
+    let convert_to_millivolts = |sample| {
+        // From http://www.st.com/resource/en/datasheet/CD00161566.pdf
+        // 5.3.4 Embedded reference voltage
+        const VREFINT_MV: u32 = 1200; // mV
+
+        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+    };
 
     loop {
-        info!("{:?}", segmts.current_state());
+        if *segmts.current_state() == segements::State::Freshed {
+            let v = convert_to_millivolts(adc.read(&mut pb1)) as usize;
+            numbers[1] = v / 1000;
+            numbers[2] = v / 100 - numbers[1] * 10;
+            numbers[3] = v / 10 - (v / 100) * 10;
+        }
         Timer::after(segmts.state_exe(&numbers)).await;
         segmts.state_trans();
     }
@@ -48,6 +67,15 @@ async fn display(
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
+    info!("Set JTag pins as normal pin.");
+    unsafe {
+        RCC.apb2enr().modify(|w| w.set_afioen(true));
+        embassy_stm32::pac::AFIO.mapr().modify(|w| {
+            w.set_swj_cfg(2);
+        });
+        RCC.apb2enr().modify(|w| w.set_afioen(false));
+    }
+
     let mut config = Config::default();
     config.rcc.hse = Some(Hertz(8_000_000));
     config.rcc.sys_ck = Some(Hertz(72_000_000));
@@ -57,23 +85,28 @@ async fn main(spawner: Spawner) -> ! {
 
     let dp = embassy_stm32::init(config);
 
+    info!("PC7, PC8, PC9 as LED Output Pins.");
     spawner.spawn(blink(dp.PC7, dp.PC8, dp.PC9)).unwrap();
 
-    let mut segmts = Segments::new(
-        dp.PB3,
-        dp.PB4,
-        dp.PB5,
-        dp.PC10,
-        dp.PC11,
-        dp.PC12,
-        Duration::from_millis(2),
-    );
-
-    let numbers = [0, 2, 1, 4, 3, 2, 6, 7];
+    info!("ADC1 use to sample voltage from PB1");
+    info!("PB3, PB4 and PB5 use to send segment data");
+    info!("PC10, PC11 and PC12 use to drive segment fresh");
+    spawner
+        .spawn(display(
+            dp.ADC1,
+            dp.PB1,
+            dp.PB3,
+            dp.PB4,
+            dp.PB5,
+            dp.PC10,
+            dp.PC11,
+            dp.PC12,
+            Duration::from_millis(2),
+        ))
+        .unwrap();
 
     loop {
-        info!("{:?}", segmts.current_state());
-        Timer::after(segmts.state_exe(&numbers)).await;
-        segmts.state_trans();
+        // info!("{:?}", segmts.current_state());
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
